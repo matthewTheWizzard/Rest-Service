ac40f1c3f8bb3b493a055b7109e2f3875bcb366e
matthewTheWizzard <mmdrumnew@gmail.com>
2023-06-01 15:09:31 +0400

    feat: add 404 error handling

--- app.js
+++ app.js
@@ -7,4 +7,19 @@ const ordersRoutes = require('./api/routes/orders')
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not Found')
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
 module.exports = app;
